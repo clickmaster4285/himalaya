c71896c0670a2dad048109e48c9ed90f9a6d7c26
Software Clickmasters <software.clickmasters@gmail.com>
2026-07-18 17:54:21 +0500

    updated

--- frontend/app/robots.ts
+++ frontend/app/robots.ts
@@ -5,10 +5,10 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
         disallow: [
           "/api/",
           "/superadmin",
+          " /admin",
         ],
       },
     ],
